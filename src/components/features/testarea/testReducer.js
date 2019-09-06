05e480d10860faf9651dd4bd751117ab5348b4a5
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-09-06 22:25:17 +0530

    add better way to create reducers

--- src/components/features/testarea/testReducer.js
+++ src/components/features/testarea/testReducer.js
@@ -1,16 +1,16 @@
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from "../../../types/Types";
+import { createReducer } from "../../../common/util/reducerUtil";
 
 const initialState = 42;
 
-const testReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case INCREMENT_COUNTER:
-      return state + 1;
-    case DECREMENT_COUNTER:
-      return state - 1;
-    default:
-      return state;
+  const incrementCounter = (state)=>{
+      return state + 1 ;
   }
-};
+  const decrementCounter = (state)=>{
+    return state - 1 ;
+}
 
-export default testReducer;
+export default createReducer(initialState, {
+    [INCREMENT_COUNTER]: incrementCounter,
+    [DECREMENT_COUNTER]: decrementCounter
+});
