50483e66bcb326930bfc3e30dfe75b65cd19beeb
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-09-06 12:43:03 +0530

    add submit and onChage methods to event form

--- src/components/features/event/EventForm/EventForm.jsx
+++ src/components/features/event/EventForm/EventForm.jsx
@@ -1,32 +1,47 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Segment, Form, Button } from 'semantic-ui-react';
 
 const EventForm = ({isOpen, hideForm})=>{
+
+    const [eventTitle, setEventTitle] = useState('');
+    const [eventDate, setEventDate] = useState('');
+    const [eventCity, setEventCity] = useState('');
+    const [eventVenue, setEventVenue] = useState('');
+    const [eventHostedBy, setEventHostedBy] = useState('');
+
+    const onChange = (e, name)=>{
+        name(e.target.value);
+    }
+
+    const onSubmit = (e)=>{
+        e.preventDefault();
+        console.log(eventTitle, eventDate, eventCity, eventVenue, eventHostedBy);
+    }
 
     const showForm = ()=>{
         if(isOpen){
             return (
                 <Segment>
-                <Form>
+                <Form onSubmit={(e)=>onSubmit(e)}>
                   <Form.Field>
                     <label>Event Title</label>
-                    <input placeholder="First Name" />
+                    <input value={eventTitle}  placeholder="Event Title" onChange={(e)=>onChange(e, setEventTitle)}/>
                   </Form.Field>
                   <Form.Field>
                     <label>Event Date</label>
-                    <input type="date" placeholder="Event Date" />
+                    <input value={eventDate} type="date" placeholder="Event Date" onChange={(e)=>onChange(e, setEventDate)}/>
                   </Form.Field>
                   <Form.Field>
                     <label>City</label>
-                    <input placeholder="City event is taking place" />
+                    <input value={eventCity} placeholder="City event is taking place" onChange={(e)=>onChange(e, setEventCity)}/>
                   </Form.Field>
                   <Form.Field>
                     <label>Venue</label>
-                    <input placeholder="Enter the Venue of the event" />
+                    <input value={eventVenue} placeholder="Enter the Venue of the event" onChange={(e)=>onChange(e, setEventVenue)}/>
                   </Form.Field>
                   <Form.Field>
                     <label>Hosted By</label>
-                    <input placeholder="Enter the name of person hosting" />
+                    <input value={eventHostedBy} placeholder="Enter the name of person hosting" onChange={(e)=>onChange(e, setEventHostedBy)}/>
                   </Form.Field>
                   <Button positive type="submit">
                     Submit
